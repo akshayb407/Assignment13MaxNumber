15fb9918287de102a36075513051a55429f834ca
akshayb407 <akshayb407@gmail.com>
2022-06-25 02:06:19 +0530

    UC5

---
+++ FindMaximumNumberProject/FindMaximumNumberProject/GenericMax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumberProject
+{
+    internal class GenericMax<T> where T : IComparable
+    {
+        ///<summary>
+        ///this is method is created for maximum to std output using using generic Method
+        ///</summary>
+        public  T firstValue, secondValue, thirdValue;
+        public GenericMax(T firstValue, T sewcondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = sewcondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+
+            }
+            return default;
+
+        }
+        public T MaxMethod()
+        {
+            T max = GenericMax<T>.MaxValue(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+    }
+}
--- FindMaximumNumberProject/FindMaximumNumberProject/Program.cs
+++ FindMaximumNumberProject/FindMaximumNumberProject/Program.cs
@@ -7,9 +7,17 @@ namespace FindMaximumNumberProject
         static void Main(string[] arg)
         {
              Console.WriteLine("Find Maximum Number");
-            int[] arr = { 112, 344, 456, 555, 876 };
-            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
-            generic.PrintMaxValues();
+
+            //int[] arr = { 112, 344, 456, 555, 876 };
+            //GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            //generic.PrintMaxValues();
+
+            var genericint = GenericMax<int>.MaxValue(20, 30, 80);
+           Console.WriteLine(genericint);
+            var genericString = GenericMax<String>.MaxValue("40", "50", "60");
+            Console.WriteLine(genericString);
+            var genericdouble = GenericMax<double>.MaxValue(20.20, 40.20, 60.20);
+            Console.WriteLine(genericdouble);
 
 
         }
